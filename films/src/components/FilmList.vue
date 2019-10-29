a8b70ef5ecfd57848abc1382bb26b7a121f09464
ankaimer <ankaimer@gmail.com>
2019-10-29 18:45:26 +0000

    film demo

---
+++ films/src/components/FilmList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <h1>Film List</h1>
+      <b-card-group columns :key="film.id" v-for="film in films">
+        <FilmCard :film="film"/>
+      </b-card-group>
+      
+  </div>
+</template>
+
+<script>
+
+import FilmCard from './FilmCard.vue'
+
+export default {
+    components: {
+        FilmCard
+    },
+
+    data: function() {
+            return { 
+                films: [
+                    {id: 1, name: 'Rocky', rating: 3, image: require('../assets/rocky.jpeg')},
+                    {id: 2, name: 'DeadPool', rating: 5, image: require('../assets/deadpool.jpeg')},
+                    {id: 3, name: 'La La Land', rating: 0, image: require('../assets/lalaland.jpeg')}
+                ]            
+            }
+    }
+    
+}
+
+</script>
+
+<style>
+
+</style>
